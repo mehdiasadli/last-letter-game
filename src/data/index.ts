f9492b4321582d1f0aed7e712da772202b4850a1
Mehdi Asadli <mehdiasadli@azericard.com>
2025-08-13 20:48:23 +0400

    add: IMDB250 list

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,15 @@
 // data of the packs
-import { chemicalElements, countries, landlockedCountries, nationalCapitals, usStates, zodiacSigns } from './packs';
+import {
+  chemicalElements,
+  countries,
+  imdbTop250,
+  landlockedCountries,
+  nationalCapitals,
+  usStates,
+  zodiacSigns,
+} from './packs';
 import type { GameData } from './types';
 
 export const data = {
-  packs: [countries, usStates, landlockedCountries, zodiacSigns, chemicalElements, nationalCapitals],
+  packs: [countries, usStates, landlockedCountries, chemicalElements, nationalCapitals, imdbTop250, zodiacSigns],
 } as const satisfies GameData;
--- src/data/packs/index.ts
+++ src/data/packs/index.ts
@@ -9,3 +9,9 @@ export * from './zodiac-signs';
 
 // SCIENCE PACKS
 export * from './chemical-elements';
+
+// ENTERTAINMENT PACKS
+export * from './imdb-top-250';
+
+// HISTORY PACKS
+// export * from './roman-emperors';
