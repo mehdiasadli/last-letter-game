8e2b28984a6d73682a248225931dc39c3b892322
Mehdi Asadli <mehdiasadli@azericard.com>
2025-08-13 17:32:17 +0400

    add: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ColorSchemeScript } from '@mantine/core';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './app';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ColorSchemeScript defaultColorScheme='auto' />
+    <Analytics />
     <App />
   </StrictMode>
 );
